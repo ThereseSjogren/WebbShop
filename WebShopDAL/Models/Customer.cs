dddecc49b433c1e0800b67aa31052392f054b347
asvimalo <asvimalo@gmail.com>
2016-09-20 15:39:49 +0200

    Fixed merge issue

---
+++ WebShopDAL/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopDAL.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Address { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int ZipCodeID { get; set; }
+        public int RabbatID { get; set; }
+
+    }
+}
---
+++ WebShopDAL/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopDAL.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int PriceUnit { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public decimal Stock { get; set; }
+
+        public int CategoryID { get; set; }
+
+
+        public Product(int productID,string productName,int priceUnit,string description, string color, string size, int stock, int categoryID)
+        {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.PriceUnit = priceUnit;
+            this.Description = description;
+            this.Color = color;
+            this.Size = size;
+            this.Stock = stock;
+            this.CategoryID = categoryID;
+
+        }
+    }
+}
